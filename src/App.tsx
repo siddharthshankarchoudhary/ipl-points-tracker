e724d7759d696b7fdc324a1ca81d3c4b0577189b
siddharthshankarchoudhary <shankarchoudharysiddharth@gmail.com>
2025-03-30 22:44:36 +0530

    Fix auth path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="auth" element={<AuthPage />} />
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/" element={<GameScoreChart />} />
       </Routes>
     </Router>
