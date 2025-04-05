39c24296bf769fe19ba6a24cb0aee7c324a5c5a9
siddharthshankarchoudhary <shankarchoudharysiddharth@gmail.com>
2025-04-06 00:29:35 +0530

    Update routes

--- src/routes/Path.ts
+++ src/routes/Path.ts
@@ -1,7 +1,7 @@
 export enum Path {
   Home = "/",
-  Dashboard = "/dashboard",
-  Profile = "/profile",
-  SignIn = "/sign-in",
-  SignUp = "/sign-up",
+  Dashboard = "dashboard",
+  Profile = "profile",
+  SignIn = "sign-in",
+  SignUp = "sign-up",
 }
