ece4479fde7c0bf6d1aa5cee2403d6db2f8f1228
siddharthshankarchoudhary <shankarchoudharysiddharth@gmail.com>
2025-04-20 00:08:48 +0530

    Update score

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -57,6 +57,7 @@ const data: MatchData[] = [
   { Match: "MI vs SRH", Arvind: -10, Nirvikar: 10, Siddharth: -10 },   
   { Match: "RCB vs PBKS", Arvind: -10, Nirvikar: 10, Siddharth: 10 },   
   { Match: "DC vs GT", Arvind: -10, Nirvikar: 10, Siddharth: -10 },   
+  { Match: "RR vs LSG", Arvind: 10, Nirvikar: -10, Siddharth: 10 },   
 ];
 
 const cumulativeData = data.map((match, index) => ({
